c4706c7dc62d91571118a705074145f9c8e680d6
lexrayne <lexrayne@outlook.com>
2023-08-06 22:09:28 -0300

    Fixed routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,6 +20,7 @@ router.get('/:id', async (req, res) => {
 
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id.' });
+      return;
     }
 
     res.status(200).json(productData);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,7 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: Product
+      include: [Product]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -15,11 +15,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: Product
+      include: [Product]
     });
 
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id.' });
+      return;
     }
 
     res.status(200).json(tagData);
@@ -49,6 +50,11 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     })
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
     
     res.status(200).json(tagData);
   } catch (err) {
@@ -68,6 +74,8 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No tag found with this id.' });
       return;
     }
+
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
